fix scaling error

ex1_linear_regression/normal.py
@@ -4,9 +4,9 @@
 
 def norm_list(l):
     assert (len(l) != 0)
-    ave = sum(l) / len(l)
     range_lst = max(l) - min(l)
-    return list(map(lambda x: (x - ave) / range_lst, l))
+    import numpy as np
+    return list(map(lambda x: (x - np.mean(l)) / range_lst, l))
 
 
 def norm_matrix(m):
@@ -14,6 +14,6 @@ def norm_matrix(m):
     row, col = np.shape(m)
 
     new_m = []
-    for c in range(1, col, 1):
-        new_m.append(norm_list(list(m[1:row, c-1: c].flat)))
-    return np.asarray(new_m)
+    for c in range(1, col + 1, 1):
+        new_m.append(norm_list(list(m[0:row, c - 1: c].flat)))
+    return np.asarray(new_m).T