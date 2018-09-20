20c64eb232e717f935a3680e9f48d975b945e75b
mincong.he <hemincong@gmail.com>
2018-09-21 00:32:48 +0800

    fix normal function

--- ex1_linear_regression/normal.py
+++ ex1_linear_regression/normal.py
@@ -4,15 +4,12 @@
 
 def norm_list(l):
     assert (len(l) != 0)
-    range_lst = max(l) - min(l)
     import numpy as np
-    return list(map(lambda x: (x - np.mean(l)) / range_lst, l))
-
+    return list(map(lambda x: (x - np.mean(l)) / np.std(l), l))
 
 def norm_matrix(m):
     import numpy as np
     row, col = np.shape(m)
-
     new_m = []
     for c in range(1, col + 1, 1):
         new_m.append(norm_list(list(m[0:row, c - 1: c].flat)))
