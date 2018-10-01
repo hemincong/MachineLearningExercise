a872925a9ecd402072b2797851bfcff4fca11355
Manchung.Ho <hemincong@gmail.com>
2018-10-02 00:26:38 +0800

    + more test case

--- ex2_logistic_regression/test/test_sigmoid.py
+++ ex2_logistic_regression/test/test_sigmoid.py
@@ -3,9 +3,12 @@
 
 import unittest
 
+
 class test_sigmoid(unittest.TestCase):
 
-    def test_sigmoid1(self):
+    def test_sigmoid(self):
         from ex2_logistic_regression.sigmoid import sigmoid
         ret = sigmoid(-1)
         self.assertAlmostEqual(ret, 0.268, delta=0.01)
+        ret = sigmoid(0)
+        self.assertAlmostEqual(ret, 0.5, delta=0.01)
