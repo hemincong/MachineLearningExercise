76602c20698be0732d604799f31276ad2e2b45e4
Manchung.Ho <hemincong@gmail.com>
2018-10-07 22:23:26 +0800

    + ex2, implement fminunc

---
+++ ex2_logistic_regression/ex2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import scipy.optimize as op
+
+
+def line_regression_by_fmin(x, y):
+    row, col = x.shape
+
+    import numpy as np
+    initial_theta = np.zeros(col)
+
+    from ex2_logistic_regression.costFunction import costFunction, compute_grad
+    return op.minimize(fun=costFunction,
+                       x0=initial_theta,
+                       args=(x, y),
+                       method='TNC',
+                       jac=compute_grad)
--- ex2_logistic_regression/test/test_cost.py
+++ ex2_logistic_regression/test/test_cost.py
@@ -2,27 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import scipy.optimize as op
 
 
 class test_cost(unittest.TestCase):
 
     def test_cost(self):
         from utils import file_utils
-        x, y = file_utils.read_csv_split_last_col("test/resource/ex2data1.txt")
+        x, y = file_utils.read_csv_split_last_col_and_add_one("test/resource/ex2data1.txt")
         from ex2_logistic_regression.costFunction import costFunction
         import numpy as np
         x_row, x_col = np.shape(x)
-        theta = np.zeros(x_col + 1)
+        theta = np.zeros(x_col)
         ret = costFunction(theta, x, y)
         self.assertAlmostEqual(ret, 0.693, delta=0.01)
 
     def test_compute_theta(self):
         from utils import file_utils
-        x, y = file_utils.read_csv_split_last_col("test/resource/ex2data1.txt")
+        x, y = file_utils.read_csv_split_last_col_and_add_one("test/resource/ex2data1.txt")
         from ex2_logistic_regression.costFunction import compute_grad
         import numpy as np
         x_row, x_col = np.shape(x)
-        theta = np.zeros(x_col + 1)
+        theta = np.zeros(x_col)
         grad = compute_grad(theta, x, y)
         self.assertAlmostEqual(grad[0], -0.1, delta=0.1)
         self.assertAlmostEqual(grad[1], -12.00, delta=0.01)
@@ -30,12 +31,19 @@ class test_cost(unittest.TestCase):
 
     def test_compute_theta_2(self):
         from utils import file_utils
-        x, y = file_utils.read_csv_split_last_col("test/resource/ex2data1.txt")
+        x, y = file_utils.read_csv_split_last_col_and_add_one("test/resource/ex2data1.txt")
         from ex2_logistic_regression.costFunction import compute_grad_2
         import numpy as np
         x_row, x_col = np.shape(x)
-        theta = np.zeros(x_col + 1)
+        theta = np.zeros(x_col)
         grad = compute_grad_2(theta, x, y)
         self.assertAlmostEqual(grad[0], -0.1, delta=0.1)
         self.assertAlmostEqual(grad[1], -12.00, delta=0.01)
         self.assertAlmostEqual(grad[2], -11.262, delta=0.01)
+
+    def test_fmin(self):
+        from utils import file_utils
+        x, y = file_utils.read_csv_split_last_col_and_add_one("test/resource/ex2data1.txt")
+        from ex2_logistic_regression.ex2 import line_regression_by_fmin
+        ret = line_regression_by_fmin(x, y)
+        self.assertAlmostEqual(ret.fun, 0.203, delta=0.01)
