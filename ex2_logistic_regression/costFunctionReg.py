6b4e46a1da9961022dee502dc2bf298e2c3ec6a3
mincong.he <hemincong@gmail.com>
2018-11-14 00:43:51 +0800

    finish cost function with regular

--- ex2_logistic_regression/costFunctionReg.py
+++ ex2_logistic_regression/costFunctionReg.py
@@ -5,10 +5,13 @@
 def costFunctionReg(theta, x, y, _lambda):
     import numpy as np
     x_row, x_col = np.shape(x)
+    from ex2_logistic_regression.mapFeature import mapFeature
     sum_l = 0.0
     from ex2_logistic_regression.sigmoid import sigmoid
     for r in range(x_row):
-        cost_row = sigmoid(theta.dot(x[r]))
+        reg_x = [1] + mapFeature(x[r][1], x[r][2])
+        print("reg_x: {reg}".format(reg=reg_x))
+        cost_row = sigmoid(theta.dot(np.asarray(reg_x)))
         y_row = y[r]
         sum_l += (-y_row) * np.log(cost_row) - (1 - y_row) * np.log(1 - cost_row)
     cost_without_reg = sum_l / x_row
@@ -16,3 +19,20 @@ def costFunctionReg(theta, x, y, _lambda):
     cost = cost_without_reg + sum(np.power(theta_without_1, 2)) * _lambda / 2 / x_row
     return cost
 
+def compute_grad_reg(theta, x, y, _lambda):
+    import numpy as np
+    x_row, x_col = np.shape(x)
+    from ex2_logistic_regression.sigmoid import sigmoid
+    grad = [0] * x_col
+    for r in range(x_row):
+        cost_row = sigmoid(theta.dot(x[r]))
+        y_row = y[r]
+        for c in range(x_col):
+            grad[c] += ((cost_row - y_row) * x[r][c])[0]
+
+    for c in range(x_col):
+        grad[c] = grad[c] / x_row
+
+    for c in range(1, x_col):
+        grad[c] = grad[c] + _lambda / x_row * theta[c]
+    return grad
--- test/test_ex2_reg.py
+++ test/test_ex2_reg.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-data_file = "resource/ex2data2.txt"
+data_file_path = "test/resource/ex2data2.txt"
 
 
 class test_ex2_reg(unittest.TestCase):
@@ -15,11 +15,26 @@ class test_ex2_reg(unittest.TestCase):
 
     def test_cost_function_reg(self):
         from ex2_logistic_regression.costFunctionReg import costFunctionReg
+        from ex2_logistic_regression.mapFeature import mapFeature
         from utils import file_utils
-        x, y = file_utils.read_csv_split_last_col_and_add_one(data_file)
+        x, y = file_utils.read_csv_split_last_col_and_add_one(data_file_path)
+        import numpy as np
+        x_row, x_col = np.shape(x)
+        theta = np.zeros(29)
+        _lambda = 1
+        cost = costFunctionReg(theta, x, y, _lambda)
+        self.assertAlmostEqual(cost, 0.693, delta=0.001)
+
+    def test_compute_grad_reg(self):
+        from utils import file_utils
+        x, y = file_utils.read_csv_split_last_col_and_add_one(data_file_path)
+        from ex2_logistic_regression.costFunctionReg import compute_grad_reg
         import numpy as np
         x_row, x_col = np.shape(x)
         theta = np.zeros(x_col)
         _lambda = 1
-        cost = costFunctionReg(theta, x, y, _lambda)
-        self.assertAlmostEqual(cost, 0.693, delta=0.001)
+        grad = compute_grad_reg(theta, x, y, _lambda)
+        print("grad: {grad}".format(grad=grad))
+        #self.assertAlmostEqual(grad[0], -0.1, delta=0.1)
+        #self.assertAlmostEqual(grad[1], -12.00, delta=0.01)
+        #self.assertAlmostEqual(grad[2], -11.262, delta=0.01)
