d36d63da7ebd12f646ef639f43f47190c77cf429
Manchung.Ho <hemincong@gmail.com>
2018-11-15 23:51:35 +0800

    rewrite mapFeature

--- ex2_logistic_regression/mapFeature.py
+++ ex2_logistic_regression/mapFeature.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def mapFeature(X1, X2):
     degree = 6
     out = []
     from math import pow
-    for i in range(degree + 2):
-        for j in range(i):
-            out.append(pow(X1, i-j) * pow(X2, j))
+    for r in range(len(X1)):
+        row = []
+        for i in range(degree + 2):
+            for j in range(i):
+                xx = pow(X1[r], i - j) * pow(X2[r], j)
+                row.append(xx)
+        out.append(row)
     return out
-
--- test/test_ex2_reg.py
+++ test/test_ex2_reg.py
@@ -3,15 +3,18 @@
 
 import unittest
 
-data_file_path = "test/resource/ex2data2.txt"
+data_file_path = "resource/ex2data2.txt"
 
 
 class test_ex2_reg(unittest.TestCase):
 
     def test_map_feature(self):
         from ex2_logistic_regression.mapFeature import mapFeature
-        ret = mapFeature(1, 2)
-        self.assertEqual(len(ret), 28)
+        from utils import file_utils
+        x, y = file_utils.read_csv_split_last_col_and_add_one(data_file_path)
+        ret = mapFeature(x[:, 1], x[:, 2])
+        self.assertEqual(len(x), len(ret))
+        self.assertEqual(len(ret[0]), 28)
 
     def test_cost_function_reg(self):
         from ex2_logistic_regression.costFunctionReg import costFunctionReg
@@ -35,6 +38,6 @@ class test_ex2_reg(unittest.TestCase):
         _lambda = 1
         grad = compute_grad_reg(theta, x, y, _lambda)
         print("grad: {grad}".format(grad=grad))
-        #self.assertAlmostEqual(grad[0], -0.1, delta=0.1)
-        #self.assertAlmostEqual(grad[1], -12.00, delta=0.01)
-        #self.assertAlmostEqual(grad[2], -11.262, delta=0.01)
+        # self.assertAlmostEqual(grad[0], -0.1, delta=0.1)
+        # self.assertAlmostEqual(grad[1], -12.00, delta=0.01)
+        # self.assertAlmostEqual(grad[2], -11.262, delta=0.01)
