02e26193c83304ccce8bc169689b1d947a33e3d1
Manchung.Ho <hemincong@gmail.com>
2019-02-26 14:43:33 +0800

    + test_displayAnImage

--- test/test_ex3.py
+++ test/test_ex3.py
@@ -6,12 +6,21 @@ import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ex3_neural_network.displayData import plot_100_image
+from ex3_neural_network.displayData import plot_100_image, plot_an_image
 
 data_file = "resource/ex3data1.mat"
 
 
 class test_ex3(unittest.TestCase):
+
+    def test_displayAnImage(self):
+        data = sio.loadmat(data_file)
+        X = data.get('X')
+        X = np.array([im.reshape((20, 20)).T for im in X])
+        # and I flat the image again to preserve the vector presentation
+        X = np.array([im.reshape(400) for im in X])
+        plot_an_image(X[0])
+        plt.show()
 
     def test_displayData(self):
         data = sio.loadmat(data_file)
