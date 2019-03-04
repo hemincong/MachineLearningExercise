6ba829f1ab838e35ad2cc4712bf888a967a98190
Manchung.Ho <hemincong@gmail.com>
2019-03-04 23:00:04 +0800

    simply code

--- ex3_neural_network/lrCostFunction.py
+++ ex3_neural_network/lrCostFunction.py
@@ -23,22 +23,11 @@ def gradient(theta, X, y, _lambda):
     return grad_reg_without_reg.flatten()
 
 
-def lrGradient(theta, X, y, _lambda):
-    m, n = X.shape
-
-    from utils.sigmoid import sigmoid
-    h = sigmoid(np.dot(X, theta))
-    t = h.T - y
-    tmp = np.dot(t, X)
-    grad_reg_without_reg = tmp / m
-    grad_reg = tmp / m + (float(_lambda) / m) * theta
-    grad_reg[0] = grad_reg_without_reg[0]
-    return grad_reg.flatten()
-
-
 def lrCostFunction(theta, X, y, _lambda):
     m, n = X.shape
 
     cost = costFunction(theta, X, y, _lambda) + (np.dot(theta[1:n], theta[1:n]) / m / 2 * _lambda)
-    grad = lrGradient(theta, X, y, _lambda)
-    return cost, grad
+    grad_reg_without_reg = gradient(theta, X, y, _lambda)
+    grad_reg = grad_reg_without_reg + (float(_lambda) / m) * theta
+    grad_reg[0] = grad_reg_without_reg[0]
+    return cost, grad_reg
