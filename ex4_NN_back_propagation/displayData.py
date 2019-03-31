d3358819f523236cf7e7c059114b6d5f242a2a60
Manchung.Ho <hemincong@gmail.com>
2019-04-01 00:53:27 +0800

    + ex4

---
+++ ex4_NN_back_propagation/displayData.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def displayData(X, example_width = None):
+    """Display 2D data in a nice grid
+    [h, display_array] = displayData(X, example_width) displays 2D data
+    stored in X in a nice grid.
+    :param X:
+    :param example_width:
+    :return: the figure handle h and the displayed array if requested.
+    """
+    # Set example_width automatically if not passed in
+    if example_width is None:
+        example_width = int(round(np.sqrt(X.shape[1])))
+
+    # Gray Image
+    colormap = 'gray'
+
+    # Compute rows, cols
+    m, n = X.shape
+    example_height = int(n / example_width)
+
+    # Compute number of items to display
+    display_rows = int(np.floor(np.sqrt(m)))
+    display_cols = int(np.ceil(m / display_rows))
+
+    # Between images padding
+    pad = 1
+
+    # Setup blank display
+    display_array = -np.ones((pad + display_rows * (example_height + pad), pad + display_cols * (example_width + pad)))
+
+    # Copy each example into a patch on the display array
+    curr_ex = 0
+    for j in range(display_rows):
+        for i in range(display_cols):
+            if curr_ex > m:
+                break
+            # Copy the patch
+
+            # Get the max value of the patch
+            max_val = max(abs(X[curr_ex, :]))
+            sample_y = pad + j * (example_height + pad)
+            sample_x = pad + i * (example_width + pad)
+            sample_pixel = X[curr_ex, :].reshape((example_height, example_width), order='F') / max_val
+            display_array[sample_y:sample_y + example_height, sample_x:sample_x + example_width] = sample_pixel
+            curr_ex += 1
+        if curr_ex > m:
+            break
+
+    # Display Image
+    h = plt.imshow(display_array, cmap=colormap)
+    # Do not show axis
+    plt.axis('off')
+
+    plt.show()
+    return h, display_array
---
+++ test/test_ex4.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# python adaptation of solved ex4.m
+#
+# Neural network learning
+#
+# depends on
+#
+#     displayData.py
+#     sigmoidGradient.py
+#     randInitializeWeights.py
+#     nnCostFunction.py
+#
+
+import scipy.io
+import numpy as np
+import unittest
+#import nnCostFunction as nncf
+#import sigmoidGradient as sg
+#import randInitializeWeights as riw
+#import checkNNGradients as cnng
+#from scipy.optimize import minimize
+#import predict as pr
+
+## Setup the parameters you will use for this exercise
+input_layer_size  = 400  # 20x20 Input Images of Digits
+hidden_layer_size = 25   # 25 hidden units
+num_labels = 10          # 10 labels, from 1 to 10
+                          # (note that we have mapped "0" to label 10)
+
+## =========== Part 1: Loading and Visualizing Data =============
+#  We start the exercise by first loading and visualizing the dataset.
+#  You will be working with a dataset that contains handwritten digits.
+#
+
+data_file = "resource/ex4data1.mat"
+
+class test_ex4_nn_back_propagation(unittest.TestCase):
+
+    def test_displayData(self):
+        import ex4_NN_back_propagation.displayData as dd
+        # Load Training Data
+        print('Loading and Visualizing Data ...')
+        mat = scipy.io.loadmat(data_file)
+        X = mat["X"]
+        y = mat["y"]
+        m = X.shape[0]
+
+        # crucial step in getting good performance!
+        # changes the dimension from (m,1) to (m,)
+        # otherwise the minimization isn't very effective...
+        y=y.flatten()
+
+        # Randomly select 100 data points to display
+        rand_indices = np.random.permutation(m)
+        sel = X[rand_indices[:100],:]
+
+        dd.displayData(sel)
+
