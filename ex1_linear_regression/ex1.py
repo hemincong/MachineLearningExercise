ff62e8e2fc2146f5afa993c1bc6d68dbce5a0b47
Manchung.Ho <hemincong@gmail.com>
2019-04-03 09:35:43 +0800

    clean code

--- ex1_linear_regression/ex1.py
+++ ex1_linear_regression/ex1.py
@@ -80,7 +80,6 @@ def drew_1():
 
 
 def drew_J_theta():
-    from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
     from matplotlib import cm
     from matplotlib.ticker import LinearLocator, FormatStrFormatter
