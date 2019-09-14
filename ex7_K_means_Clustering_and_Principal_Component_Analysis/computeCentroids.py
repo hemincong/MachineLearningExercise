b65c81c6f7f60eb17accae2926a1da5dfcc35183
Manchung.Ho <hemincong@gmail.com>
2019-09-14 11:15:33 +0800

    + computeCentroids and its test

---
+++ ex7_K_means_Clustering_and_Principal_Component_Analysis/computeCentroids.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def computeCentroids(X, idx, K):
+    # COMPUTECENTROIDS returs the new centroids by computing the means of the
+    # data points assigned to each centroid.
+    #   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
+    #   computing the means of the data points assigned to each centroid. It is
+    #   given a dataset X where each row is a single data point, a vector
+    #   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+    #   example, and K, the number of centroids. You should return a matrix
+    #   centroids, where each row of centroids is the mean of the data points
+    #   assigned to it.
+    #
+
+    # Useful variables
+    m, n = X.shape
+
+    # You need to return the following variables correctly.
+    centroids = np.zeros((K, n))
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Go over every centroid and compute mean of all points that
+    #               belong to it. Concretely, the row vector centroids(i, :)
+    #               should contain the mean of the data points assigned to
+    #               centroid i.
+    #
+    # Note: You can use a for-loop over the centroids to compute this.
+    #
+
+    for i in range(K):
+        filtered_X = X[np.nonzero(idx == i)[0]]
+        centroids[i] = np.mean(filtered_X, axis=0)
+
+    return centroids
--- test/test_ex7.py
+++ test/test_ex7.py
@@ -51,7 +51,7 @@ class test_ex6_svm(unittest.TestCase):
         idx = findClosestCentroids(self.X, initial_centroids)
 
         print('Closest centroids for the first 3 examples:')
-        print(' {idx}'.format(idx=idx))
+        # print(' {idx}'.format(idx=idx))
         # adjusted next string for python's 0-indexing
         print('(the closest centroids should be 0, 2, 1 respectively)')
 
@@ -59,3 +59,25 @@ class test_ex6_svm(unittest.TestCase):
         self.assertEqual(idx.flatten()[0], 0)
         self.assertEqual(idx.flatten()[1], 2)
         self.assertEqual(idx.flatten()[2], 1)
+        # ===================== Part 2: Compute Means =========================
+        #  After implementing the closest centroids function, you should now
+        #  complete the computeCentroids function.
+        #
+        print('Computing centroids means.')
+
+        #  Compute means based on the closest centroids found in the previous part.
+        from ex7_K_means_Clustering_and_Principal_Component_Analysis.computeCentroids import computeCentroids
+        centroids = computeCentroids(self.X, idx, K)
+
+        print('Centroids computed after initial finding of closest centroids: ')
+        print(' {centroids} '.format(centroids=centroids))
+        print('(the centroids should be')
+        print('   [ 2.428301 3.157924 ]')
+        print('   [ 5.813503 2.633656 ]')
+        print('   [ 7.119387 3.616684 ]')
+        self.assertAlmostEqual(centroids[0, :][0], 2.429301, delta=0.001)
+        self.assertAlmostEqual(centroids[0, :][1], 3.157924, delta=0.001)
+        self.assertAlmostEqual(centroids[1, :][0], 5.813503, delta=0.001)
+        self.assertAlmostEqual(centroids[1, :][1], 2.633656, delta=0.001)
+        self.assertAlmostEqual(centroids[2, :][0], 7.119387, delta=0.001)
+        self.assertAlmostEqual(centroids[2, :][1], 3.616684, delta=0.001)
